42f2d470c45033b13b83e8da1364b12c3c4507f4
alves-dev <alvesmoreiraigor@gmail.com>
2025-07-24 15:38:15 -0300

    feat: refactored device infos

--- custom_components/stackspot/util.py
+++ custom_components/stackspot/util.py
@@ -3,8 +3,8 @@ from homeassistant.helpers.device_registry import DeviceInfo, DeviceEntryType
 from .const import DOMAIN, INTEGRATION_NAME
 
 
-def get_device_info(entry_id: str, name: str) -> DeviceInfo:
-    device_identifier = f'{entry_id}_{name}'
+def get_device_info_agent(entry_id: str, name: str) -> DeviceInfo:
+    device_identifier = f'stackspot_agent_device_{entry_id}'
     return DeviceInfo(
         identifiers={(DOMAIN, device_identifier)},
         name=name,
@@ -13,10 +13,11 @@ def get_device_info(entry_id: str, name: str) -> DeviceInfo:
     )
 
 
-def get_device_general() -> DeviceInfo:
+def get_device_general(entry_id: str) -> DeviceInfo:
+    device_identifier = f'stackspot_general_device_{entry_id}'
     return DeviceInfo(
-        identifiers={(DOMAIN, 'stackspot_global_device')},
-        name='Stackspot AI - Config',
+        identifiers={(DOMAIN, device_identifier)},
+        name='Config',
         manufacturer=INTEGRATION_NAME,
         entry_type=DeviceEntryType.SERVICE
     )
