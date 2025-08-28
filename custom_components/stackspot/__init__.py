d0a02640b3b3ce3ba9535a348ad7c1e7ebe1c9a7
alves-dev <alvesmoreiraigor@gmail.com>
2025-08-28 10:18:01 -0300

    feat: new variable 'exposed_entities' for prompts

--- custom_components/stackspot/__init__.py
+++ custom_components/stackspot/__init__.py
@@ -1,8 +1,10 @@
 import logging
+from datetime import timedelta, datetime
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.event import async_track_time_interval
 
 from .const import (
     DOMAIN,
@@ -13,6 +15,7 @@ from .const import (
 )
 from .entities.stackspot_entity_manager import StackSpotEntityManager
 from .sensor import TokenTotalSensor
+from .util import get_list_exposed_entities
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,6 +31,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN][MANAGER] = StackSpotEntityManager()
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await process_exposed_entities(hass)
 
     return True
 
@@ -36,3 +40,18 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     return unload_ok
+
+
+async def process_exposed_entities(hass: HomeAssistant) -> None:
+    manager: StackSpotEntityManager = hass.data[DOMAIN][MANAGER]
+    key = 'exposed-entities-task'
+
+    remove_listener = manager.get_object_by(key)
+    if remove_listener is not None:
+        remove_listener()
+
+    async def task(now: datetime) -> None:
+        await get_list_exposed_entities(hass)
+
+    remove_listener = async_track_time_interval(hass, task, timedelta(minutes=5))
+    manager.add_objetc(key, remove_listener)
